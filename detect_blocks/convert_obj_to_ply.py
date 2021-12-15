43a47d785a26664117d7b84cd848bd96d01e0ab0
Pavel Grunt <pavel.grunt@goodai.com>
2021-12-15 15:38:31 +0100

    Fix obj to ply conversion
    Remove cube from the scene
    
    Use ascii export - required by auto-pose

--- detect_blocks/convert_obj_to_ply.py
+++ detect_blocks/convert_obj_to_ply.py
@@ -16,5 +16,8 @@ with open(os.path.join(DATASET_PATH, 'metadata_files.txt')) as file:
                 print('Skipping {}: file exists'.format(model_path_ply))
                 continue
             bpy.ops.import_scene.obj(filepath=model_path)
-            bpy.ops.export_mesh.ply(filepath=model_path_ply)
+            objs = bpy.data.objects
+            if "Cube" in objs:
+                objs.remove(objs["Cube"], do_unlink=True)
+            bpy.ops.export_mesh.ply(filepath=model_path_ply, use_ascii=True)
             bpy.ops.object.delete()
